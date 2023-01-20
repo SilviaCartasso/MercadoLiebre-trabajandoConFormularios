c2d8b9477c638fee89a1fcd4fdc50ae12c155d1c
SilviaCartasso <smc.03673@gmail.com>
2023-01-20 14:32:24 -0300

    se sube practica integradora

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require ("express");
+const app = express ();
+const path = require ("path");
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.listen (port, () => {
+    return console.log(`Server listen in ${port}
+    http://localhost:${port}`)
+});
+
+app.get("/", (req, res) =>{
+    return res.sendFile(path.join(__dirname, "views/home.html"))
+});
+
+app.get("/register", (req, res) =>{
+    return res.sendFile(path.join(__dirname, "views/register.html"))
+});
+
+app.get("/login", (req, res) =>{
+    return res.sendFile(path.join(__dirname, "views/login.html"))
+});
